89f6f1f150609e7dd5d9f9630ea2be39e9a146b3
Sherzod Nurjonov <Sherzod_Nurjonov@epam.com>
2020-12-07 16:45:31 +0500

    added module eureka server

--- ws-yoke-notify-master/src/main/java/com/epam/yoke/notify/YokeNotifyApplication.java
+++ ws-yoke-notify-master/src/main/java/com/epam/yoke/notify/YokeNotifyApplication.java
@@ -2,8 +2,10 @@ package com.epam.yoke.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class YokeNotifyApplication {
 
   public static void main(String[] args) {
