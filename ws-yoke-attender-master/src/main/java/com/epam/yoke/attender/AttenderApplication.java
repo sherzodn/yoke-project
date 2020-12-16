0cc354dc0aa783947c6c3644b6f7955a3a65dbf7
Sherzod Nurjonov <Sherzod_Nurjonov@epam.com>
2020-12-17 00:00:42 +0500

    Added Load Balancer

--- ws-yoke-attender-master/src/main/java/com/epam/yoke/attender/AttenderApplication.java
+++ ws-yoke-attender-master/src/main/java/com/epam/yoke/attender/AttenderApplication.java
@@ -3,6 +3,7 @@ package com.epam.yoke.attender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class AttenderApplication {
 
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
